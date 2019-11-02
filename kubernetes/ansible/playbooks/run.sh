b998b749a61307a3ea57cc3d261caa7d820b2f66
Your Name <you@example.com>
2019-11-02 21:40:37 +0900

    kubectl add

--- kubernetes/ansible/playbooks/run.sh
+++ kubernetes/ansible/playbooks/run.sh
@@ -1,3 +1,14 @@
 ansible -m ping -i hosts server --ask-pass
 
 ansible-playbook ./playbook.yml -i hosts --ask-pass
+
+
+curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+mkdir ~/.kube
+scp -o "StrictHostKeyChecking no" root@192.168.56.101:/root/.kube/config ~/.kube/config
